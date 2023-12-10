a972b0b696201c1d5945133d5ff2a06399b1f6a6
KANGPUNGYUN <zkdvnd@naver.com>
2023-12-10 23:50:03 +0900

    feat: button component

---
+++ src/app/components/index.ts
@@ -0,0 +1,22 @@
+// LAYOUT
+//------------------------------------------------------------------------------
+export { Box } from "./box";
+export * from "./box.props";
+export { Flex } from "./flex";
+export * from "./flex.props";
+
+// TYPOGRAPHY
+//------------------------------------------------------------------------------
+export { Heading } from "./heading";
+export * from "./heading.props";
+export { Text } from "./text";
+export * from "./text.props";
+
+// COMPONENTS
+//------------------------------------------------------------------------------
+export { Button } from "./button";
+export * from "./base-button.props";
+export { Link } from "./link";
+export * from "./link.props";
+export { Tabs, TabsRoot, TabsList, TabsTrigger, TabsContent } from "./tabs";
+export * from "./tabs.props";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,207 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import * as React from "react";
+import styles from "./playground.module.css";
+import {
+  Box,
+  Button,
+  baseButtonPropDefs,
+  Flex,
+  Heading,
+  Link,
+  //
+  TabsRoot,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "./components";
+import { ArrowRightIcon } from "./icon";
+import { themeAccentColorsOrdered } from "./theme-options";
+import NextLink from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main>
+      <PlaygroundSection>
+        <Flex align="baseline" gap="4">
+          <Heading id="button">
+            <Link color="gray" underline="hover" highContrast href="#button">
+              Button
+            </Link>
+          </Heading>
+          <NextLink
+            passHref
+            legacyBehavior
+            href="/themes/docs/components/button"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <Link className={styles.PlaygroundDocsLink} size="2">
+              View in docs
+            </Link>
+          </NextLink>
+        </Flex>
+        <TabsRoot defaultValue="theme-colors">
+          <TabsList size="2">
+            <TabsTrigger value="theme-colors">Theme colors</TabsTrigger>
+            <TabsTrigger value="all-colors">All colors</TabsTrigger>
+            <TabsTrigger value="all-sizes">All sizes</TabsTrigger>
+          </TabsList>
+          <TabsContent value="theme-colors">
+            <Box mt="6">
+              <table className={styles.PlaygroundTable}>
+                <thead>
+                  <tr>
+                    <th />
+                    <th colSpan={2}>Accent</th>
+                    <th colSpan={2}>Gray</th>
+                    <th>Disabled</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {baseButtonPropDefs.variant.values.map((variant) => (
+                    <tr key={variant}>
+                      <td>{upperFirst(variant)}</td>
+                      <td>
+                        <Button variant={variant}>
+                          Next <ArrowRightIcon width="16" height="16" />
+                        </Button>
+                      </td>
+                      <td>
+                        <Button variant={variant} highContrast>
+                          Next <ArrowRightIcon width="16" height="16" />
+                        </Button>
+                      </td>
+                      <td>
+                        <Button variant={variant} color="gray">
+                          Next <ArrowRightIcon width="16" height="16" />
+                        </Button>
+                      </td>
+                      <td>
+                        <Button variant={variant} color="gray" highContrast>
+                          Next <ArrowRightIcon width="16" height="16" />
+                        </Button>
+                      </td>
+                      <td>
+                        <Button variant={variant} disabled>
+                          Next <ArrowRightIcon width="16" height="16" />
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </Box>
+          </TabsContent>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <TabsContent value="all-colors">
+            <Box mt="6">
+              <table className={styles.PlaygroundTable}>
+                <thead>
+                  <tr>
+                    <th />
+                    {baseButtonPropDefs.variant.values.map((variant) => (
+                      <th key={variant}>{upperFirst(variant)}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {themeAccentColorsOrdered.map((color) => (
+                    <tr key={color}>
+                      <td>{upperFirst(color)}</td>
+                      {baseButtonPropDefs.variant.values.map((variant) => (
+                        <td key={variant}>
+                          <Flex align="center" justify="center" gap="4">
+                            <Button variant={variant} color={color}>
+                              Next <ArrowRightIcon width="16" height="16" />
+                            </Button>
+                            <Button
+                              variant={variant}
+                              color={color}
+                              highContrast
+                            >
+                              Next <ArrowRightIcon width="16" height="16" />
+                            </Button>
+                          </Flex>
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </Box>
+          </TabsContent>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <TabsContent value="all-sizes">
+            <Box mt="6">
+              <table className={styles.PlaygroundTable}>
+                <thead>
+                  <tr>
+                    <th />
+                    {baseButtonPropDefs.radius.values.map((radius) => (
+                      <th key={radius} style={{ textAlign: "left" }}>
+                        {radius === "none" ? "No radius" : upperFirst(radius)}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {baseButtonPropDefs.variant.values.map((variant, index) => (
+                    <React.Fragment key={variant}>
+                      {index > 0 && (
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                      )}
+                      {baseButtonPropDefs.size.values.map((size) => (
+                        <tr key={size}>
+                          <td>Size {size}</td>
+                          {baseButtonPropDefs.radius.values.map((radius) => (
+                            <td key={radius} style={{ textAlign: "left" }}>
+                              <Button
+                                size={size}
+                                variant={variant}
+                                radius={radius}
+                              >
+                                Next{" "}
+                                <ArrowRightIcon
+                                  {...buttonSizeToIconSize(size)}
+                                />
+                              </Button>
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
+                    </React.Fragment>
+                  ))}
+                </tbody>
+              </table>
+            </Box>
+          </TabsContent>
+        </TabsRoot>
+      </PlaygroundSection>
     </main>
-  )
+  );
 }
+
+function upperFirst(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function buttonSizeToIconSize(
+  buttonSize: (typeof baseButtonPropDefs.size.values)[number]
+) {
+  if (buttonSize === "1" || buttonSize === "2")
+    return { width: 16, height: 16 };
+  if (buttonSize === "3") return { width: 18, height: 18 };
+  if (buttonSize === "4") return { width: 20, height: 20 };
+}
+
+const PlaygroundSection: React.FC<React.PropsWithChildren<unknown>> = ({
+  children,
+}) => (
+  <Flex
+    className={styles.PlaygroundSection}
+    direction="column"
+    gap="5"
+    mb={{ initial: "5", sm: "8" }}
+  >
+    {children}
+  </Flex>
+);
